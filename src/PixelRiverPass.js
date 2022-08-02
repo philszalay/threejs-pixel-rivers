294057010dbd60be6d61b1bac5b3a45e5b44a275
philszalay <philszalay@aol.de>
2022-08-02 23:01:37 +0200

    finish first stable version

---
+++ src/PixelRiverPass.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+
+const CustomPass = {
+
+  uniforms: {
+    tDiffuse: { value: null },
+    tSize: { value: new THREE.Vector2(256, 256) },
+    center: { value: new THREE.Vector2(0.5, 0.5) },
+    angle: { value: 1.57 },
+    scale: { value: 1 },
+    uTime: { value: 0 },
+    uTouchTexture: { value: null },
+    uDimensions: { value: null },
+    colorXFactor: { value: 0 },
+    colorYFactor: { value: 0 }
+  },
+
+  vertexShader:
+    `varying vec2 vUv;
+
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      vUv = uv;
+    }`,
+
+  fragmentShader:
+    `uniform vec2 center;
+    uniform float angle;
+    uniform float scale;
+    uniform vec2 tSize;
+    uniform float uTime;
+    uniform sampler2D uTouchTexture;
+    uniform vec2 uDimensions;
+    uniform float colorXFactor;
+    uniform float colorYFactor;
+    uniform float riverColorFactor;
+    uniform sampler2D tDiffuse;
+
+    varying vec2 vUv;
+
+    void main() {
+      float mouseDistortion = texture2D(uTouchTexture, vec2((gl_FragCoord.x / uDimensions.x) / 2.0, (gl_FragCoord.y / uDimensions.y) / 2.0)).r;
+      vec2 p = 2.0 * vUv - vec2(1.0, 1.0);
+      p += 0.1 * cos(scale * 2.0 * p.yx + uTime + vec2(1.2, 3.4));
+      p += 0.1 * cos(scale * 2.7 * p.yx + 1.4 * uTime + vec2(1.2, 3.4));
+      p += 0.1 * cos(scale * 3.0 * p.yx + 2.0 * uTime + vec2(2.2, 1.4));
+      p += 0.2 * cos(scale * 2.0 * p.yx + 2.6 * uTime + vec2(10.2, 3.4));
+      p += mouseDistortion * 2.;
+      vec2 newUv = vec2(mix(vUv.x, length(p), 1. + 0.0), mix(vUv.y, 0.0, 1.));
+      vec4 color = texture2D(tDiffuse, newUv);
+      color.y = color.y + (mouseDistortion / p.x) * colorYFactor;
+      color.x = color.x + (mouseDistortion / p.y) * colorXFactor;
+      gl_FragColor = color;
+    }`
+}
+
+export { CustomPass }
--- src/script.js
+++ src/script.js
@@ -1,26 +1,18 @@
 import './style.css'
+
 import * as THREE from 'three'
-import Stats from 'three/examples/jsm/libs/stats.module'
-
-import fragmentShader from './shader/fragment.glsl'
-import vertexShader from './shader/vertex.glsl'
-
+import TouchTexture from './TouchTexture'
 import image1 from '../assets/images/test1.jpg'
 import image2 from '../assets/images/test2.jpg'
 import image3 from '../assets/images/test3.jpg'
 
-import TouchTexture from './TouchTexture'
+import fragmentShader from './shader/fragment.glsl'
+import vertexShader from './shader/vertex.glsl'
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-import { CustomPass } from './customPass.js'
+import { CustomPass as PixelRiverPass } from './PixelRiverPass.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-
-/**
- * Base
- */
-
-// TODO: LoadingManager
 
 // Variables
 const sizes = {
@@ -35,24 +27,15 @@ const colors = {
 // Scene
 const scene = new THREE.Scene()
 
-// Stats
-const stats = Stats()
-stats.domElement.style.position = 'absolute'
-stats.domElement.style.top = '0px'
-
 // TouchTexture
 const touchTexture = new TouchTexture(128)
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-})
-
+const renderer = new THREE.WebGLRenderer()
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setSize(sizes.width, sizes.height)
 renderer.setClearColor(colors.rendererClearColor, 1)
-renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
-renderer.xr.enabled = true
 document.body.appendChild(renderer.domElement)
 
 // Camera
@@ -62,14 +45,13 @@ const camera = new THREE.PerspectiveCamera(
   0.001,
   1000
 )
-
 camera.position.z = 5
 
 // Postprocessing
 const composer = new EffectComposer(renderer)
 composer.addPass(new RenderPass(scene, camera))
 
-const customShaderPass = new ShaderPass(CustomPass)
+const customShaderPass = new ShaderPass(PixelRiverPass)
 customShaderPass.uniforms.uTouchTexture.value = touchTexture.texture
 customShaderPass.uniforms.uDimensions.value = new THREE.Vector2(sizes.width, sizes.height)
 composer.addPass(customShaderPass)
@@ -81,10 +63,10 @@ const textures = [image1, image2, image3].map(url => {
   return textureLoader.load(url)
 })
 
-// Geometries
+// Geometry
 const geometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1)
 
-// Objects
+// Meshes
 const images = new THREE.Group()
 
 textures.forEach((texture, index) => {
@@ -109,12 +91,14 @@ textures.forEach((texture, index) => {
   images.add(mesh)
 })
 
+images.position.z = 0.5
 scene.add(images)
 
 // Listeners
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
+  customShaderPass.uniforms.uDimensions.value = new THREE.Vector2(sizes.width, sizes.height)
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
 
@@ -123,15 +107,12 @@ window.addEventListener('resize', () => {
 
 window.addEventListener('mousemove', onMouseMove, false)
 
-const mousePosition = {
-  width: 0,
-  height: 0
-}
+const mousePosition = new THREE.Vector2(0, 0)
 
 function onMouseMove (e) {
-  mousePosition.width = e.clientX
-  mousePosition.height = e.clientY
-  touchTexture.addTouch(touchTexture.getTouchTexturePosition(sizes.width, sizes.height, mousePosition.width, mousePosition.height), 1)
+  mousePosition.x = e.clientX
+  mousePosition.y = e.clientY
+  touchTexture.addTouch(touchTexture.getTouchTexturePosition(sizes.width, sizes.height, mousePosition.x, mousePosition.y), 1)
 }
 
 // Sliders
@@ -166,10 +147,7 @@ const tick = () => {
   time += 0.01
 
   customShaderPass.uniforms.uTime.value = time
-  // touchTexture.addTouch(touchTexture.getTouchTexturePosition(sizes.width, sizes.height, mousePosition.width, mousePosition.height), 1)
-
   touchTexture.update()
-  stats.update()
   requestAnimationFrame(tick)
   composer.render()
 }
